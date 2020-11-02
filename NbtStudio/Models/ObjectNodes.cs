f95a90923c027ae9341bc90acb5b81b9f8600bb2
tryashtar <tryashtar@gmail.com>
2020-11-02 00:20:51 -0700

    Node capabilities created

--- NbtStudio/Models/ObjectNodes.cs
+++ NbtStudio/Models/ObjectNodes.cs
@@ -1,8 +1,10 @@
 ﻿using fNbt;
 using NbtStudio;
+using NbtStudio.SNBT;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -126,6 +128,50 @@ namespace NbtStudio
         public virtual bool CanEdit => false;
     }
 
+    public static class NbtNodeOperations
+    {
+        public static string Copy(INbtTag tag)
+        {
+            return tag.ToSnbt(include_name: true);
+        }
+
+        public static bool CanEdit(INbtTag tag)
+        {
+            return true;
+        }
+
+        public static bool CanPaste(INbtTag tag)
+        {
+            return tag is INbtContainer;
+        }
+
+        public static bool CanRename(INbtTag tag)
+        {
+            return !(tag.Parent is INbtList);
+        }
+
+        public static bool CanSort(INbtTag tag)
+        {
+            return tag is INbtCompound;
+        }
+
+        public static void Sort(INbtTag tag)
+        {
+            if (tag is INbtCompound compound)
+                NbtUtil.Sort(compound, new NbtUtil.TagTypeSorter(), true);
+        }
+
+        public static IEnumerable<NbtTag> ParseTags(string text)
+        {
+            var snbts = text.Split('\n');
+            foreach (var nbt in snbts)
+            {
+                if (SnbtParser.TryParse(nbt, true, out NbtTag tag) || SnbtParser.TryParse(nbt, false, out tag))
+                    yield return tag;
+            }
+        }
+    }
+
     public class NbtTagNode : NotifyNode
     {
         public readonly NotifyNbtTag Tag;
@@ -145,6 +191,21 @@ namespace NbtStudio
         {
             PerformAction(action);
         }
+
+        public override string Description => Tag.TagDescription();
+
+        public override bool CanCopy => true;
+        public override string Copy() => NbtNodeOperations.Copy(Tag);
+        public override bool CanDelete => true;
+        public override void Delete()
+        {
+            Tag.Parent?.Remove(Tag);
+        }
+        public override bool CanEdit => NbtNodeOperations.CanEdit(Tag);
+        public override bool CanPaste => NbtNodeOperations.CanPaste(Tag);
+        public override bool CanRename => NbtNodeOperations.CanRename(Tag);
+        public override bool CanSort => NbtNodeOperations.CanSort(Tag);
+        public override void Sort() => NbtNodeOperations.Sort(Tag);
     }
 
     public class NbtFileNode : NotifyNode
@@ -166,6 +227,22 @@ namespace NbtStudio
         {
             PerformAction(action);
         }
+
+        public override string Description => Path.GetFileName(File.Path);
+
+        public override bool CanCopy => true;
+        public override string Copy() => NbtNodeOperations.Copy(File.RootTag);
+        public override bool CanDelete => true;
+        public override void Delete()
+        {
+            if (File.Path != null)
+                System.IO.File.Delete(File.Path);
+        }
+        public override bool CanEdit => false;
+        public override bool CanPaste => NbtNodeOperations.CanPaste(File.RootTag);
+        public override bool CanRename => false;
+        public override bool CanSort => NbtNodeOperations.CanSort(File.RootTag);
+        public override void Sort() => NbtNodeOperations.Sort(File.RootTag);
     }
 
     public class ChunkNode : NotifyNode
@@ -178,6 +255,13 @@ namespace NbtStudio
             Chunk.Data.ActionPrepared += RootTag_ActionPrepared;
         }
 
+        public NotifyNbtCompound AccessChunkData()
+        {
+            if (!Chunk.IsLoaded)
+                Chunk.Load();
+            return Chunk.Data;
+        }
+
         private void RootTag_Changed(object sender, EventArgs e)
         {
             Notify((NotifyNbtTag)sender);
@@ -187,6 +271,21 @@ namespace NbtStudio
         {
             PerformAction(action);
         }
+
+        public override string Description => NbtUtil.ChunkDescription(Chunk);
+
+        public override bool CanCopy => true;
+        public override string Copy() => NbtNodeOperations.Copy(AccessChunkData());
+        public override bool CanDelete => true;
+        public override void Delete()
+        {
+            Chunk.Region.RemoveChunk(Chunk.X, Chunk.Z);
+        }
+        public override bool CanEdit => true;
+        public override bool CanPaste => true;
+        public override bool CanRename => true;
+        public override bool CanSort => true;
+        public override void Sort() => NbtNodeOperations.Sort(AccessChunkData());
     }
 
     public class RegionFileNode : NotifyNode
@@ -196,6 +295,20 @@ namespace NbtStudio
         {
             Region = region;
         }
+
+        public override string Description => Path.GetFileName(Region.Path);
+
+        public override bool CanCopy => false;
+        public override bool CanDelete => true;
+        public override void Delete()
+        {
+            if (Region.Path != null)
+                File.Delete(Region.Path);
+        }
+        public override bool CanEdit => false;
+        public override bool CanPaste => true;
+        public override bool CanRename => false;
+        public override bool CanSort => false;
     }
 
     public class FolderNode : NotifyNode
@@ -205,5 +318,19 @@ namespace NbtStudio
         {
             Folder = folder;
         }
+
+        public override string Description => Path.GetFileName(Folder.Path);
+
+        public override bool CanCopy => true;
+        public override bool CanDelete => true;
+        public override void Delete()
+        {
+            if (Folder.Path != null)
+                Directory.Delete(Folder.Path, true);
+        }
+        public override bool CanEdit => false;
+        public override bool CanPaste => true;
+        public override bool CanRename => false;
+        public override bool CanSort => false;
     }
 }
